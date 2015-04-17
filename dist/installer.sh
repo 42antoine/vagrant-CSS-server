ec5bc7fe5aaad9cae5be015405128ad0fe7d852e
Antoine Benevaut <antoine@cavaencoreparlerdebits.fr>
2015-04-17 14:20:11 +0200

    Update readme.

--- dist/installer.sh
+++ dist/installer.sh
@@ -5,7 +5,7 @@ spawn /home/vagrant/cssserver install
 expect "Continue" { send "y\r" }
 expect "Was the install successful?"  { send "y\r" }
 expect "Do you want to install GameServerQuery?"  { send "y\r" }
-expect "Enter server name:"  { send "vagrant cs:go\r" }
+expect "Enter server name:"  { send "vagrant cs:source\r" }
 expect "Enter rcon password:"  { send "vagrant\r" }
 
 interact
