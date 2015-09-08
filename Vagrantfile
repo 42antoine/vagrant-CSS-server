5c5a7223ca1c7de9ef5ddb6e5e9cc658bbfeb3f6
Antoine Benevaut <antoine@cavaencoreparlerdebits.fr>
2015-09-08 20:44:37 +0200

    Advancement on vagrant initialisation - auto install css server - game server web interface base installation

--- Vagrantfile
+++ Vagrantfile
@@ -9,21 +9,23 @@ Vagrant.configure(2) do |config|
   config.vm.box = "vagrant-debian78-64"
 
   # Box URL
-  config.vm.box_url = "http://cavaencoreparlerdebits.fr/pub/vagrant/boxes/vagrant-debian78-64/package.box"
+  config.vm.box_url = "http://pub.cvepdb.fr/vagrant/boxes/vagrant-debian78-64/package.box"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine.
 
   # Accessing "localhost:27915" will access port 27015 on the guest machine.
   config.vm.network "forwarded_port", guest: 27015, host: 27915
+  config.vm.network "forwarded_port", guest: 80, host: 8088
 
   # Share an additional folder to the guest VM. The first argument is
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.synced_folder "./www", "/home/vagrant/www"
+  config.vm.synced_folder "./css", "/home/vagrant/css"
 
   # Provision script
-  config.vm.provision "shell", path: "shell.sh"
+  config.vm.provision "shell", path: "shell.sh", privileged: false
 
 end
--- shell.sh
+++ shell.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
+# Variables
+DBHOST=localhost
+DBNAME=rokket
+DBUSER=root
+DBPASSWD=vagrant
+
 echo "Processing server installation"
+
 sudo apt-get update -y -qq > /dev/null
 sudo apt-get upgrade -y -qq > /dev/null
+
+# i386 packages
 sudo dpkg --add-architecture i386 > /dev/null
-sudo apt-get install gdb mailutils postfix tmux ca-certificates lib32gcc1 ia32-libs expect -y -qq > /dev/null
+sudo apt-get update -y -qq > /dev/null
+sudo apt-get install libc6-i386 lib32gcc1 -y -qq > /dev/null
+
+# i386 packages
+sudo dpkg --add-architecture i386 > /dev/null
+sudo apt-get update -y -qq > /dev/null
+sudo aptitude install ia32-libs -y -q=9 > /dev/null
+
+sudo apt-get install gdb tmux expect apache2 php5-common libapache2-mod-php5 php5-cli git -y -qq > /dev/null
+
+# PHPmyadmin
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+apt-get -y install mysql-server-5.5 phpmyadmin > /dev/null 2>&1
+
+# Force dependencies
 sudo apt-get install -f -y -qq
 
-echo "Install CS:GO server"
+/*
+ * Database config
+ */
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
+
+/*
+ * Webinterface
+ */
+ 
+sudo rm -rf /var/www
 cd /home/vagrant
+mkdir www
+sudo ln -s /home/vagrant/www /var/www
+cd /home/vagrant/www
+git clone https://github.com/aaroniker/rokket.git
+mv rokket/* . && mv rokket/.* . && rmdir rokket
+
+/*
+ * Game server
+ */
+
+echo "Install CS:GO server"
+
+cd /home/vagrant
+
 wget https://raw.github.com/dgibbs64/linuxgameservers/master/CounterStrikeSource/cssserver
-chmod +x cssserver
-wget https://raw.githubusercontent.com/42antoine/vagrant-CSS-server/dev/dist/installer.sh
-chmod +x installer.sh
-./installer.sh
+chmod +x csgoserver
+
+/*
+ * IPTables
+ */
+
+ 
+/home/vagrant/cssserver auto-install
